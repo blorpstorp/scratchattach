e572661879c866430ce4ac43730a82c215dc6f86
blorpstorp <108922356+blorpstorp@users.noreply.github.com>
2024-12-08 02:25:28 -0500

    Update main.py

--- scratchattach-test/main.py
+++ scratchattach-test/main.py
@@ -2,7 +2,7 @@ import scratchattach as sa
 import os
 import json
 
-session = sa.login_by_id("SESSID", username="System_X")
+session = sa.login_by_id(".eJxVkEFvgzAMhf8L58FCQiDtrZOmatI0rXSH7RSZxEBKSRAEoW3af18i9dKLZb1nf8_yb7IuOFsYMdkn5-_F4yg_k4fEuwFtkDjTGiqCipW6yJE3LShAFTqqSKj744d1p3VKz5eXmh-hfu_SU_6ads_LFjBX1xmbmimQKBEZExmlPBMsWBJW38uYLo0OfkkKVpScBEtfwHZOejPij7PxssOIs1Hw-Iab_HLzcL_fw9KHoYZUbY4EBdtVLW1ahYQjz3cCgTPKdcUE5CWJ4R4Xr5wbTIRvAYj6HtmACg-Id0UNrQ_p3jib3Ywlq3G63sSn2_DfP5-8bEc:1tJgno:tk_HIoq_NbcZopJ-wEivgYTRX4w", username="System_X")
 cloud = session.connect_cloud("1100926253")
 client = cloud.requests()
 
